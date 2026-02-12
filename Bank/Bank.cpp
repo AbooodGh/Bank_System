bde0d81afbbff94bce5f341e271d8e500df5630d
Abood_Gh <agh43751@gmail.com>
2026-02-12 16:37:16 +0200

    Clean up commented code in Bank.cpp
    Removed commented-out code related to client operations.

--- Bank/Bank.cpp
+++ Bank/Bank.cpp
@@ -41,39 +41,7 @@ int main()
 		{
 			break;
 		}
-	}
-
 	
-
-//	clsMainScreen::ShowMainMenue();
-
-	//UpdateClient();
-
-	//clsMainScreen::ShowMainMenue();
-
-	//AddNewClient();
-
-	//DeleteClient();
-	//ShowClientsList();
-
-	//clsBankClient Client1 = clsBankClient::Find("A101");
-	//Client1.Print();
-
-	//clsBankClient Client2 = clsBankClient::Find("A1011");
-	//Client2.Print();
-
-	//cout << endl << clsBankClient::IsClientEcist("A101") << endl;
-
-//	PrintFile("Users.txt");
-
-//	fstream MyFile;
-//	MyFile.open("DoIt.txt", ios::out);
-//	if (MyFile.is_open())
-//	{
-//		MyFile << "Adli#//#Haddad#//#Adli@gmail.com#//#8983883#//#User1#//#3456#//#-1";
-//		MyFile.close();
-//	
-//	}
 
 
 }
